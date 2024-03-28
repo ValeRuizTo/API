62a90d83dc27175cd0355a6ddbf7227d80c32103
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-03-28 17:38:21 -0500

    api

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.use(express.json())
+
 app.listen(
     PORT,
     () => console.log(`It is alive on http://localhost:${PORT}`)
 );
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 'large'
+    })
+});
+
+
+app.post('/tshirt/:id', (req, res) => { 
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message:'we need a logo' })
+    }
+
+    res.send({
+        tshirt: `👕 with your ${logo} and ID of ${id}`,
+    });
+});
